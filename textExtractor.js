fba5aae07d4bdd67d11ac0daa28bdc659953fa8b
Jose Prous <Jose.Prous@personal.com.py>
2018-01-24 16:31:40 -0300

    add P tag, keep page title

--- textExtractor.js
+++ textExtractor.js
@@ -1,37 +1,49 @@
-const NODE_TYPE_TEXT = 3;
 
 function extractText(inNode,outNode){
+    var out = outNode;
+    if(inNode.tagName == "P"){
+        var p = document.createElement("P");
+        out = p;
+    }
     var children = inNode.childNodes;
     var array = Array.from(children);
     array.forEach(function(item){
-        if(item.nodeType == NODE_TYPE_TEXT){
-            outNode.appendChild(item);
+        if(item.nodeType == 3/*text node*/){
+            out.appendChild(item);
         }else{
-            extractText(item,outNode);
+            extractText(item,out);
         }
     });
+    if(inNode.tagName == "P"){
+        outNode.appendChild(out);
+    }
 }
 
 
+var oldHead = document.head.innerHTML;
+var oldTitle = document.title;
 var oldBody = document.body.innerHTML;
 
-var oldTitle = document.getElementsByTagName("h1")[0];
 
-var title = document.createElement('h1');
-title.textContent = oldTitle.textContent;
+var oldArticleTitle = document.getElementsByTagName("h1")[0];
+
+var articleTitle = document.createElement('h1');
+articleTitle.textContent = oldArticleTitle.textContent;
 
 var oldArticleBody = document.getElementsByClassName("row article-body")[0];
 var articleBody = document.createElement("div");
 
 extractText(oldArticleBody,articleBody);
 
+document.head.innerHTML = "";
 document.body.innerHTML = "";
-
-document.body.appendChild(title);
+document.title = oldTitle;
+document.body.appendChild(articleTitle);
 document.body.appendChild(articleBody);
 
 browser.runtime.onMessage.addListener((message) => {
     if (message.command === "reset") {
+        document.head.innerHTML = oldHead;
         document.body.innerHTML = oldBody;
     }
 });
